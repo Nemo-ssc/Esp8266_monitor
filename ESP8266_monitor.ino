57f9d2d95125a7976980ae2e9d053ae206037504
Nemo-ssc <38537636+Nemo-ssc@users.noreply.github.com>
2020-06-23 13:51:28 +0800

    Update ESP8266_monitor.ino

--- ESP8266_monitor.ino
+++ ESP8266_monitor.ino
@@ -1,7 +1,7 @@
 /**********************************************************************
- * 项目：bilibili粉丝数监视器
+ * 项目：ESP8266_monitor
  * 硬件：适用于NodeMCU ESP8266 + ssd1306 128*32
- * 功能：连接WiFi后获取指定用户的哔哩哔哩实时粉丝数
+ * 功能：连接WiFi后获取指定用户的哔哩哔哩实时粉丝数+WIFI网络授时时钟+知心天气
  * 作者：Nemo_ssc  bilibili UID:12772522
  * 日期：2020/06/20
  **********************************************************************/
@@ -25,15 +25,15 @@ DHT dht(DHTPIN, DHTTYPE);
 U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
 //---------------wifi连接/API用户名密码信息----------------
-const char *ssid = "Honor V10";          //WiFi名
-const char *password = "12345678";  //WiFi密码
+const char *ssid = "********";          //WiFi名
+const char *password = "********";  //WiFi密码
 
-String biliuid = "12772522";         //bilibili UID
+String biliuid = "********";         //bilibili UID
 
 const char* HOST = "http://api.seniverse.com";
-const char* APIKEY = "SuQDmnIsehmcwH7cx";        //API KEY
-const char* CITY = "taizhou";
-const char* LANGUAGE = "zh-Hans";//zh-Hans 简体中文  会显示乱码
+const char* APIKEY = "********";        //API KEY 知心天气私钥
+const char* CITY = "********";  //城市全拼
+const char* LANGUAGE = "zh-Hans";//zh-Hans 简体中文 
 //-------------------------------------------------------
 
 //-----------------------天气参数-------------------------
@@ -53,17 +53,17 @@ weather_date day0,day1,day2;
 //-------------------------------------------------------
 
 //-----------------------定义常量-------------------------
-int Click = 0;
-int beep = 13;
+int Click = 0;  //轻触开关引脚
+int beep = 13;  //蜂鸣器引脚
 const unsigned long HTTP_TIMEOUT = 5000;  //http访问请求
 WiFiClient client;
 HTTPClient http;
 
-String response_bff;  //json获取解析
-String response_bfv;
-int follower = 0;
-long viewer = 0;
-long likes = 0;
+String response_bff;  //json返回粉丝数
+String response_bfv;  //json返回观看数、点赞数
+int follower = 0;  //粉丝数
+long viewer = 0;  //浏览量
+long likes = 0;  //点赞数
 const int slaveSelect = 5;
 const int scanLimit = 7;
 
@@ -74,8 +74,8 @@ unsigned int localPort = 8888; // 用于侦听UDP数据包的本地端口
 time_t getNtpTime();
 boolean isNTPConnected = false;
 
-String response_ws;
-String GetUrl;
+String response_ws;  //json返回天气信息
+String GetUrl;  //心知天气url请求
 //------------------------------------------------------------------
 
 //---------------------------位图信息--------------------------------
@@ -120,7 +120,8 @@ const unsigned char bilibilitv_12832[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};       
 
-//Sunny = 0
+//下面是天气图标和对应心知天气的天气代码
+//Sunny = 0 晴
 const unsigned char Sunny[] U8X8_PROGMEM ={
 0x00,0xC0,0x00,0x00,0x00,0xC0,0x00,0x00,0x00,0xC0,0x00,0x00,0x00,0xC0,0x00,0x00,
 0x30,0xC0,0x00,0x03,0x70,0x00,0x80,0x03,0xE0,0x00,0xC0,0x01,0xC0,0xE0,0xC1,0x00,
@@ -131,7 +132,7 @@ const unsigned char Sunny[] U8X8_PROGMEM ={
 0x70,0x00,0x80,0x03,0x30,0x00,0x00,0x03,0x00,0xC0,0x00,0x00,0x00,0xC0,0x00,0x00,
 0x00,0xC0,0x00,0x00,0x00,0xC0,0x00,0x00                                            
 };
-//Cloudy = 4
+//Cloudy = 4 多云
 const unsigned char Cloudy[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x0E,0x00,
 0x00,0x80,0x3F,0x00,0x00,0xE6,0xFF,0x00,0x00,0xFF,0xFF,0x00,0x80,0xFF,0xFF,0x00,
@@ -142,7 +143,7 @@ const unsigned char Cloudy[] U8X8_PROGMEM ={
 0x7E,0xF0,0xFF,0x1F,0x7F,0xE0,0xFF,0x0F,0x3E,0xC0,0xFF,0x07,0x00,0x00,0x00,0x00,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00                                            
 };
-//Overcast = 9
+//Overcast = 9 阴
 const unsigned char Overcast[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 0x00,0x00,0x00,0x00,0x00,0x80,0x1F,0x00,0x00,0xE0,0x7F,0x00,0x00,0xF3,0xFF,0x00,
@@ -153,7 +154,7 @@ const unsigned char Overcast[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00                                      
 };
-//Shower = 10
+//Shower = 10 阵雨
 const unsigned char Shower[] U8X8_PROGMEM ={
 0x00,0x00,0x70,0x00,0x00,0x00,0xFC,0x03,0x00,0x00,0xFE,0x07,0x00,0x00,0xF8,0x0F,
 0x00,0xC0,0xE3,0x1F,0x00,0xF0,0xCF,0x1F,0xC0,0xF9,0x9F,0x3F,0xE0,0xFF,0xBF,0x3F,
@@ -164,7 +165,7 @@ const unsigned char Shower[] U8X8_PROGMEM ={
 0x60,0x60,0x30,0x00,0x60,0x60,0x30,0x00,0x30,0x30,0x18,0x00,0x30,0x30,0x18,0x00,
 0x18,0x18,0x0C,0x00,0x08,0x08,0x04,0x00                                           
 };
-//Thundershower = 11
+//Thundershower = 11 雷阵雨
 const unsigned char Thundershower[] U8X8_PROGMEM ={
 0x00,0x00,0x0E,0x00,0x00,0xC0,0x3F,0x00,0x00,0xE0,0xFF,0x00,0xC0,0xFF,0xFF,0x01,
 0xE0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x03,0xF0,0xFF,0xFF,0x03,
@@ -175,7 +176,7 @@ const unsigned char Thundershower[] U8X8_PROGMEM ={
 0x30,0xFC,0x60,0x00,0x30,0x70,0x60,0x00,0x10,0x30,0x20,0x00,0x10,0x18,0x20,0x00,
 0x00,0x08,0x00,0x00,0x00,0x00,0x00,0x00                                           
 };
-//Rain_L = 13
+//Rain_L = 13 小雨
 const unsigned char Rain_L[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x0E,0x00,0x00,0xC0,0x3F,0x00,0x00,0xE0,0xFF,0x00,
 0xC0,0xFF,0xFF,0x01,0xE0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x03,
@@ -186,7 +187,7 @@ const unsigned char Rain_L[] U8X8_PROGMEM ={
 0x00,0x60,0x00,0x00,0x00,0x20,0x00,0x00,0x00,0x30,0x00,0x00,0x00,0x10,0x00,0x00,
 0x00,0x18,0x00,0x00,0x00,0x00,0x00,0x00                                            
 };
-//Rain_M = 14
+//Rain_M = 14 中雨
 const unsigned char Rain_M[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x0E,0x00,0x00,0xC0,0x3F,0x00,0x00,0xE0,0xFF,0x00,
 0xC0,0xFF,0xFF,0x01,0xE0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x03,
@@ -197,7 +198,7 @@ const unsigned char Rain_M[] U8X8_PROGMEM ={
 0x00,0x0C,0x0C,0x00,0x00,0x04,0x04,0x00,0x00,0x06,0x06,0x00,0x00,0x02,0x02,0x00,
 0x00,0x03,0x03,0x00,0x00,0x00,0x00,0x00                                   
 };
-//Rain_H = 15
+//Rain_H = 15 大雨
 const unsigned char Rain_H[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x0E,0x00,0x00,0xC0,0x3F,0x00,0x00,0xE0,0xFF,0x00,
 0xC0,0xFF,0xFF,0x01,0xE0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x01,0xF0,0xFF,0xFF,0x03,
@@ -208,7 +209,7 @@ const unsigned char Rain_H[] U8X8_PROGMEM ={
 0x60,0x60,0x60,0x00,0x20,0x20,0x20,0x00,0x30,0x30,0x30,0x00,0x10,0x10,0x10,0x00,
 0x18,0x18,0x18,0x00,0x00,0x00,0x00,0x00                                            
 };
-//Foggy = 30
+//Foggy = 30 雾
 const unsigned char Foggy[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x1E,0x00,0x00,0xC0,0xFF,0x00,0x00,0xF0,0xFF,0x01,0x00,
 0xF8,0xFF,0x03,0x00,0xFC,0xFF,0x07,0x00,0xFC,0xFF,0x0F,0x00,0xFE,0xFF,0xFF,0x01,
@@ -219,7 +220,7 @@ const unsigned char Foggy[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xFE,0xFF,0xFF,0x1F,
 0xFE,0xFF,0xFF,0x1F,0x00,0x00,0x00,0x00                                            
 };
-//Haze = 31
+//Haze = 31 霾
 const unsigned char Haze[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x0E,0xE0,0x00,0x1C,0x0E,0xE0,0x00,0x1C,
 0x0E,0xE0,0x00,0x1C,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
@@ -231,6 +232,7 @@ const unsigned char Haze[] U8X8_PROGMEM ={
 0x0E,0xE0,0x00,0x1C,0x00,0x00,0x00,0x00                                            
 };
 
+//浏览和点赞的图标
 //viewer
 const unsigned char viewer_bmp[] U8X8_PROGMEM ={
 0x00,0x00,0x00,0x00,0x80,0x00,0x8C,0x18,0x98,0x0C,0xF0,0x07,0x38,0x0E,0x0C,0x18,
@@ -246,12 +248,9 @@ const unsigned char likes_bmp[] U8X8_PROGMEM ={
 //------------------------------------------------------------------
 ICACHE_RAM_ATTR void onChange()
 {
-    delay(100);
+    delay(100);  //延时消抖
     if(digitalRead(15) == HIGH)
     {
-        digitalWrite(beep, HIGH);
-        delay(70);
-        digitalWrite(beep, LOW);
         Click ++;
         Serial.println("Key Down");
     }
@@ -262,19 +261,19 @@ ICACHE_RAM_ATTR void onChange()
 //------------------------------------------------------------------
 void setup()
 {
-    attachInterrupt(15, onChange, RISING);//中断控制 引脚为内部GPIO口号
+    attachInterrupt(15, onChange, RISING);  //中断控制 引脚为内部GPIO口号
     pinMode(beep, OUTPUT);
-    u8g2.begin();
+    u8g2.begin();  //启用u8g2库
     u8g2.clearBuffer();
-    u8g2.drawXBMP( 0 , 0 , 128 , 32 , bilibilitv_12832 );
+    u8g2.drawXBMP( 0 , 0 , 128 , 32 , bilibilitv_12832 );  //开机画面
     u8g2.sendBuffer();
-    Serial.begin(115200);
+    Serial.begin(115200);  //串口通讯波特率
     while (!Serial)
         continue;
     Serial.println("bilibili fans monitor, version v1.2");
     Serial.print("Connecting WiFi...");
     WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);  //连接WIFI
     while (WiFi.status() != WL_CONNECTED){
         delay(500);
         Serial.print(".");
@@ -298,7 +297,7 @@ void setup()
 //------------------------------------------------------------------
 //--------------------bilibili_fans_follower------------------------
 //------------------------------------------------------------------
-bool getJson()
+bool getJson()  //同时获得粉丝数和浏览量的json数据
 {
     bool r = false;
     http.setTimeout(HTTP_TIMEOUT);
@@ -336,7 +335,7 @@ bool getJson()
     return r;
 }
 
-bool parseJson_bff(String json)
+bool parseJson_bff(String json)   //对粉丝数的json解析
 {
     const size_t capacity = JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 70;//分配内存大小
     DynamicJsonDocument doc(capacity);  //解析Json
@@ -371,7 +370,7 @@ bool parseJson_bff(String json)
     return true;
 }
 
-void displayNumber_f(int number) 
+void displayNumber_f(int number)  //oled输出粉丝数
 {
     u8g2.setFont(u8g2_font_freedoomr25_tn); // choose a suitable font
     u8g2.setCursor(40, 30);
@@ -379,13 +378,13 @@ void displayNumber_f(int number)
     u8g2.sendBuffer();          // transfer internal memory to the display
 }
 
-void displayBmp_f()
+void displayBmp_f()  //输出图片
 {
     u8g2.clearBuffer();
     u8g2.drawXBMP( 4 , 4 , 24 , 24 , bilibilitv_24u );
 }
 
-void bilibili_fans_follower()
+void bilibili_fans_follower()  //获取json数据并解析成功
 {
     if (getJson()){
         if (parseJson_bff(response_bff)){
@@ -414,7 +413,7 @@ void bilibili_fans_follower()
 //--------------------bilibili_fans_viewer--------------------------
 //------------------------------------------------------------------
 
-bool parseJson_bfv(String json)
+bool parseJson_bfv(String json)  //对浏览量的json解析
 {
     const size_t capacity = 2*JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + 70;
     DynamicJsonDocument doc(capacity);
@@ -433,7 +432,7 @@ bool parseJson_bfv(String json)
     likes = data_likes;
 }
 
-void displayBmp_v()
+void displayBmp_v()  //输出图片
 {
     u8g2.clearBuffer();
     u8g2.drawXBMP( 4 , 4 , 24 , 24 , bilibilitv_24u );  
@@ -441,7 +440,7 @@ void displayBmp_v()
     u8g2.drawXBMP( 100 , 17 , 15 , 15 , likes_bmp );
 }
 
-void displayNumber_v(int vw,int lk)
+void displayNumber_v(int vw,int lk)  //oled输出浏览量和点赞数
 {
     u8g2.setFont(u8g2_font_freedoomr10_tu); // choose a suitable font
     u8g2.setCursor(40, 15);
@@ -451,7 +450,7 @@ void displayNumber_v(int vw,int lk)
     u8g2.sendBuffer();
 }
 
-void bilibili_fans_viewer()
+void bilibili_fans_viewer()   //获取json数据并解析成功
 {    
     if (getJson()){
         if (parseJson_bfv(response_bfv)){
@@ -524,7 +523,7 @@ void sendNTPpacket(IPAddress& address)
   Udp.write(packetBuffer, NTP_PACKET_SIZE);
   Udp.endPacket();
 }
-void NTP_CLOCK()
+void NTP_CLOCK()  //oled输出时分秒，没有十位的补0
 {
     int hours, minutes, seconds;
     hours = hour();
@@ -556,7 +555,7 @@ void NTP_CLOCK()
     u8g2.print("UTC+8");
     u8g2.sendBuffer();
 }
-void NTP_DAY()
+void NTP_DAY()  //oled输出年月日+星期
 {
     int years, months, days, weekdays;
     years = year();
@@ -602,7 +601,7 @@ void NTP_DAY()
 //----------------------------api test------------------------------
 //https://api.seniverse.com/v3/weather/now.json?key=SuQDmnIsehmcwH7cx&location=beijing&language=zh-Hans&unit=c
 //https://api.seniverse.com/v3/weather/daily.json?key=SuQDmnIsehmcwH7cx&location=beijing&language=zh-Hans&unit=c&start=0&days=5
-bool getJson_ws()
+bool getJson_ws()  //构建并发出获取天气数据的请求，获得json数据
 {
     bool s = false;
     GetUrl = String(HOST) + "/v3/weather/daily.json?key=";
@@ -629,15 +628,15 @@ bool getJson_ws()
     return s;
 }
 
-bool parseJson_ws(String json)
+bool parseJson_ws(String json)  //对心知天气的json数据解析，采用arduinojson v6库
 {
     const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(3) + JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(6) + 3*JSON_OBJECT_SIZE(14) + 810;
-    DynamicJsonDocument doc(capacity);
-    deserializeJson(doc, json);
+    DynamicJsonDocument doc(capacity);  //根据json数据结构计算所需要的内存大小
+    deserializeJson(doc, json);  //反序列化json数据
     JsonObject results_0 = doc["results"][0];
     JsonArray results_0_daily = results_0["daily"];
 
-    JsonObject results_0_daily_0 = results_0_daily[0];
+    JsonObject results_0_daily_0 = results_0_daily[0];  //今天的天气数据
     const char* results_0_daily_0_date = results_0_daily_0["date"]; // "2020-06-20"
     const char* results_0_daily_0_text_day = results_0_daily_0["text_day"]; // "多云"
     const char* results_0_daily_0_code_day = results_0_daily_0["code_day"]; // "4"
@@ -653,7 +652,7 @@ bool parseJson_ws(String json)
     const char* results_0_daily_0_wind_scale = results_0_daily_0["wind_scale"]; // "3"
     const char* results_0_daily_0_humidity = results_0_daily_0["humidity"]; // "72"
 
-    JsonObject results_0_daily_1 = results_0_daily[1];
+    JsonObject results_0_daily_1 = results_0_daily[1];  //明天的天气数据
     const char* results_0_daily_1_date = results_0_daily_1["date"]; // "2020-06-21"
     const char* results_0_daily_1_text_day = results_0_daily_1["text_day"]; // "多云"
     const char* results_0_daily_1_code_day = results_0_daily_1["code_day"]; // "4"
@@ -669,7 +668,7 @@ bool parseJson_ws(String json)
     const char* results_0_daily_1_wind_scale = results_0_daily_1["wind_scale"]; // "3"
     const char* results_0_daily_1_humidity = results_0_daily_1["humidity"]; // "68"
 
-    JsonObject results_0_daily_2 = results_0_daily[2];
+    JsonObject results_0_daily_2 = results_0_daily[2];  //后天的天气数据
     const char* results_0_daily_2_date = results_0_daily_2["date"]; // "2020-06-22"
     const char* results_0_daily_2_text_day = results_0_daily_2["text_day"]; // "中雨"
     const char* results_0_daily_2_code_day = results_0_daily_2["code_day"]; // "14"
@@ -685,20 +684,20 @@ bool parseJson_ws(String json)
     const char* results_0_daily_2_wind_scale = results_0_daily_2["wind_scale"]; // "4"
     const char* results_0_daily_2_humidity = results_0_daily_2["humidity"]; // "71"
     
-    const char* results_0_last_update = results_0["last_update"];
+    const char* results_0_last_update = results_0["last_update"];  //数据更新时间
     
     String date0 = results_0_daily_0_date;  //将日期取出处理
     String date1 = results_0_daily_1_date;  
     String date2 = results_0_daily_2_date; 
     
-    day0.date_m = date0.substring(5, 7);
+    day0.date_m = date0.substring(5, 7);  //日期字符串切片
     day0.date_d = date0.substring(8, 10);
     day0.code_day = atoi(results_0_daily_0_code_day);//获取今天天气信息
-    day0.high = atoi(results_0_daily_0_high);
-    day0.low = atoi(results_0_daily_0_low);
-    day0.wind_direction_degree = atoi(results_0_daily_0_wind_direction_degree);
-    day0.wind_scale = atoi(results_0_daily_0_wind_scale);
-    day0.humidity = atoi(results_0_daily_0_humidity);
+    day0.high = atoi(results_0_daily_0_high);  //最高温度
+    day0.low = atoi(results_0_daily_0_low);  //最低温度
+    day0.wind_direction_degree = atoi(results_0_daily_0_wind_direction_degree);  //风向
+    day0.wind_scale = atoi(results_0_daily_0_wind_scale);  //风力等级
+    day0.humidity = atoi(results_0_daily_0_humidity);  //湿度
     
     day1.date_m = date1.substring(5, 7);
     day1.date_d = date1.substring(8, 10);
@@ -721,7 +720,7 @@ bool parseJson_ws(String json)
     return true;
 }
 
-void display_today()
+void display_today()  //oled显示今天的天气信息
 {   
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_lubB08_tr); 
@@ -743,7 +742,7 @@ void display_today()
     //u8g2.setCursor(80, 31);
     //u8g2.print(day0.wind_direction_degree);
     
-    String Direction;
+    String Direction;  //将风向角度简化为风向文字信息
     if (day0.wind_direction_degree == 0)
     {
         Direction = "N";
@@ -784,7 +783,7 @@ void display_today()
     u8g2.print(day0.wind_scale);
 
     
-    switch(day0.code_day)
+    switch(day0.code_day)  //将天气代码转化为天气图标和字符显示
     {
         case 0:
             u8g2.drawXBMP( 1 , 1 , 30 , 30 , Sunny );
@@ -842,7 +841,7 @@ void display_today()
     Serial.println(day0.code_day);
 }
 
-void weather_station0()
+void weather_station0()  //获取解析json，显示今天天气
 {   
     if (getJson_ws()){
         if (parseJson_ws(response_ws)){
@@ -850,10 +849,9 @@ void weather_station0()
         }
     
     }
-    //delay(5000);  //5秒请求一次
 }
 
-void weather_station1()
+void weather_station1()  //并列显示三天日期和最高/低温度信息
 {
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_lubB08_tr); 
@@ -906,7 +904,7 @@ void weather_station1()
     u8g2.sendBuffer();
 }
 
-void weather_station2()
+void weather_station2()  //并列显示三天天气图标
 {   
     u8g2.clearBuffer();
     switch(day0.code_day)
@@ -1015,7 +1013,7 @@ void weather_station2()
 //------------------------------------------------------------------
 //-------------------------DHT11_SENSOR-----------------------------
 //------------------------------------------------------------------
-void dht11()
+void dht11()  //DHT11传感器，精度不行已弃用
 {
     float h = dht.readHumidity();
     // Read temperature as Celsius (the default)
@@ -1052,7 +1050,7 @@ void dht11()
 //------------------------------------------------------------------
 //---------------------------loop函数--------------------------------
 //------------------------------------------------------------------
-void loop()
+void loop()  //主循环，判断按键次数切换运行程序和显示
 {
     int menu = Click%7;
     if (WiFi.status() == WL_CONNECTED){
@@ -1086,6 +1084,6 @@ void loop()
         Serial.println(Click);
     }
     else{
-        Serial.println("[WiFi] Waiting to reconnect...");
+        Serial.println("[WiFi] Waiting to reconnect...");  //未联网串口输出
     }
 }
